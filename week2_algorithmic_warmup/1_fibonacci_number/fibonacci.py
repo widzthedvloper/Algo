ab19ac40645de27e8f1057c3f388d4d5c8217c88
Widz <widz@gmail.com>
2023-10-12 13:30:07 -0800

    Calculate fibo

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -4,7 +4,18 @@ def fibonacci_number(n):
 
     return fibonacci_number(n - 1) + fibonacci_number(n - 2)
 
+def fibo_number(n):
+    arr = [None for i in range(n + 1)]
+    if n <= 1:
+        return n
+    arr[0] = 0
+    arr[1] = 1
+    for i in range(2, n + 1):
+        arr[i] = arr[i -1] + arr[i - 2]
+
+    return arr[n]
+
 
 if __name__ == '__main__':
     input_n = int(input())
-    print(fibonacci_number(input_n))
+    print(fibo_number(input_n))
