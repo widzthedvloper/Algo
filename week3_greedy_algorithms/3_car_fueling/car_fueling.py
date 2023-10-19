64079ba783b3989731d9ee1a5a47de42ca8bef69
Widz <widz@gmail.com>
2023-10-19 12:40:37 -0800

    Solution for car fueling

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -3,7 +3,21 @@ from sys import stdin
 
 def min_refills(distance, tank, stops):
     # write your code here
-    return -1
+    refill_amount = 0
+    tank_miles = tank
+    new_stops = stops.append(distance - (len(stops) - 1))
+    for i in range(len(new_stops) - 2):
+        next_stop_distance = new_stops[i + 1]
+        tank_miles -= new_stops[1]
+
+        if tank_miles < next_stop_distance:
+            if tank_miles + tank > next_stop_distance:
+                return -1
+            refill_amount += 1
+        
+
+
+    return refill_amount
 
 
 if __name__ == '__main__':
