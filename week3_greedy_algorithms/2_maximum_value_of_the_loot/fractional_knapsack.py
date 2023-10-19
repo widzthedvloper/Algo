ce5e5f19035ebfbc96dc0349776b5f882a72a0c2
Widz <widz@gmail.com>
2023-10-19 11:51:37 -0800

    Solution for knapsack

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -3,7 +3,25 @@ from sys import stdin
 
 def optimal_value(capacity, weights, values):
     value = 0.
+    cap = capacity
     # write your code here
+    values_per_weights = {}
+    values_per_weights_arr = []
+    for i in range(len(weights)):
+        val_per_weight = values[i] / weights[i]
+        values_per_weights_arr.append(val_per_weight)
+        values_per_weights[val_per_weight] = [values[i], weights[i]]
+
+    values_per_weights_arr.sort()
+
+    for val_per_w in values_per_weights_arr:
+        val, wei = values_per_weights[val_per_w]
+        if wei <= cap and cap < 0:
+            value += wei * val_per_w
+            cap -= val
+        elif wei > cap:
+            value += cap * val_per_w
+            cap = 0
 
     return value
 
